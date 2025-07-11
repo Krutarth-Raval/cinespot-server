10cd2020f5a8599ab2dc0fc5f4bbd25ed7ff9361
Krutarth-Raval <ravalkrutarth95@gmail.com>
2025-07-11 19:32:43 +0530

    update frontend url

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import collectionRoutes from "./routes/collectionRoutes.js";
 const app = express();
 const port = process.env.PORT || 4000;
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://cinemaspot.netlify.app'];
 
 app.use(express.json());
 app.use(cookieParser());
