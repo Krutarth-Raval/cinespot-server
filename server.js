a8b389ca3cfe1b183d4837fa5cf6be4f46022ef6
Krutarth-Raval <ravalkrutarth95@gmail.com>
2025-07-16 12:52:43 +0530

    server file updated

--- server.js
+++ server.js
@@ -10,11 +10,24 @@ import collectionRoutes from "./routes/collectionRoutes.js";
 const app = express();
 const port = process.env.PORT || 4000;
 
-const allowedOrigins = ['http://localhost:5173', 'https://cinemaspot.netlify.app'];
+
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = ['http://localhost:5173', 'https://cinemaspot.netlify.app'];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 connectDB();
 
 //API endpoints
